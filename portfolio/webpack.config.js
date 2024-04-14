d7768f0f320dc12b52b7a47863cb586fa78c201c
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2024-04-14 20:48:05 +0900

    webpack setting

--- portfolio/webpack.config.js
+++ portfolio/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/public/",
+    publicPath: "/",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
