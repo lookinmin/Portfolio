988390357bbcccc52d3eadb98d34f010de0743bb
lookinmin <ancx1234@naver.com>
2025-03-05 21:51:14 +0900

    career 수정정

--- portfolio/src/Components/Career.tsx
+++ portfolio/src/Components/Career.tsx
@@ -10,16 +10,16 @@ export const Career = () => {
         <p>CAREERS</p>
       </div>
 
-      <CareerBox>
+      {/* <CareerBox>
         <p>서울우유협동조합</p>
         <p>2025.02 ~</p>
         <hr />
         <p>근무형태 : 인턴</p>
-        <p>소속속 : 경영정보팀</p>
+        <p>소속 : 경영정보팀</p>
         <hr />
         <p>[주 업무]</p>
         <li></li>
-      </CareerBox>
+      </CareerBox> */}
 
       <CareerBox>
         <p>남박사로봇코딩학원 (청주)</p>
