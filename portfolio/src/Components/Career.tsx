7bb0f65b295707728da7e259fb46d220767efa0e
lookinmin <ancx1234@naver.com>
2025-04-01 22:18:40 +0900

    Career - 서울우유 추가가

--- portfolio/src/Components/Career.tsx
+++ portfolio/src/Components/Career.tsx
@@ -10,25 +10,37 @@ export const Career = () => {
         <p>CAREERS</p>
       </div>
 
-      {/* <CareerBox>
+      <CareerBox>
         <p>서울우유협동조합</p>
-        <p>2025.02 ~</p>
+        <p>2025.02.17 ~ (근무중)</p>
         <hr />
-        <p>근무형태 : 인턴</p>
+        <p>근무 형태 : 인턴</p>
         <p>소속 : 경영정보팀</p>
         <hr />
-        <p>[주 업무]</p>
-        <li></li>
-      </CareerBox> */}
+        <p style={{ color: '#c7e5ff' }}>[개발 환경 및 활용 언어]</p>
+        <li>Oracle 12C</li>
+        <li>OracleERP 12.2.7</li>
+        <li>MyBuilder, Webcrea</li>
+        <li>SVN, Jenkins</li>
+        <li>Oracle9i Designer</li>
+        <li>OZ Designer, Report</li>
+        <hr />
+        <p style={{ color: '#c7e5ff' }}>[주 업무]</p>
+        <li>AP(채무) 모듈 관련 업무 진행</li>
+        <li>PO(구매) 모듈 관련 업무 진행</li>
+        <li>TAX(세무) 모듈 관련 업무 진행</li>
+        <li>직원 공장도가 신청 및 조회 화면 개발</li>
+        <li>업무 알림 시스템 개발</li>
+      </CareerBox>
 
       <CareerBox>
         <p>남박사로봇코딩학원 (청주)</p>
-        <p>2022.07 ~ 2023.10</p>
+        <p>2022.07.16 ~ 2023.10.14</p>
         <hr />
-        <p>근무형태 : 아르바이트</p>
+        <p>근무 형태 : 아르바이트</p>
         <p>직책 : 코딩 강사</p>
         <hr />
-        <p>[주 업무]</p>
+        <p style={{ color: '#c7e5ff' }}>[주 업무]</p>
         <li>C, C++, Python 등 기초 프로그래밍 언어 강의</li>
         <li>코딩 올림피아드 대비 알고리즘 문제 해설 강의</li>
         <li>HTML5/CSS3/JavaScript 웹 프로그래밍 기초 강의</li>
